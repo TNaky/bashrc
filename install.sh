f0c43b3261e3b0dad429a2685336463c125090ab
TNak@MacMini <michibata.dev@gmail.com>
2016-09-28 15:35:12 +0900

    bash_profileを設定するように編集

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://github.com/TNaky/bashrc.git ${HOME}/.bash
-ln -s .bash/bashrc ${HOME}/.bashrc
+ln -s ${HOME}/.bash/bashrc ${HOME}/.bashrc
+ln -s ${HOME}/.bash/bash_profile ${HOME}/.bash_profile
