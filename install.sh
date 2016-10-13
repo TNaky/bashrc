3d7f12909fa44cb59f292b610a78ce69e8188ecc
TNak@MacMini <michibata.dev@gmail.com>
2016-10-13 18:25:05 +0900

    gitがインストールされていない場合の処理を追記

--- install.sh
+++ install.sh
@@ -6,35 +6,41 @@ if [ "$(uname)" == "Darwin" ]; then
   echo -e '  \033[00;34m'http://brew.sh/index_ja.html'\033[00m'
 fi
 
-if [ -d ${HOME} ]; then
-  git -C ${HOME}/.bash/.git pull
+if [[ $(type git 2> /dev/null) ]]; then
+  if [ -d ${HOME} ]; then
+    git -C ${HOME}/.bash/.git pull
+  else
+    git clone https://github.com/TNaky/bashrc.git ${HOME}/.bash
+  fi
+  
+  if [ -f "${HOME}/.bashrc" ]; then
+    mv ${HOME}/.bashrc ${HOME}/.bash/bashrc.orig
+    ln -s ${HOME}/.bash/bashrc ${HOME}/.bashrc
+    echo "Original .bashrc file has been moved to"
+    echo -e '  \033[01;33m'\"${HOME}/.bash/bashrc.orig\"'\033[00m'
+  else
+    ln -s ${HOME}/.bash/bashrc ${HOME}/.bashrc
+  fi
+  
+  if [ -f "${HOME}/.bash_profile" ]; then
+    mv ${HOME}/.bash_profile ${HOME}/.bash/bash_profile.orig
+    ln -s ${HOME}/.bash/bash_profile ${HOME}/.bash_profile
+    echo "Original .bash_profile file has been moved to"
+    echo -e '  \033[01;33m'\"${HOME}/.bash/bash_profile.orig\"'\033[00m'
+  else
+    ln -s ${HOME}/.bash/bash_profile ${HOME}/.bash_profile
+  fi
+  
+  if [ -f "${HOME}/.inputrc " ]; then
+    mv ${HOME}/.inputrc ${HOME}/.bash/inputrc.orig
+    ln -s ${HOME}/.bash/inputrc ${HOME}/.inputrc
+    echo "Original .inputrc file has been moved to"
+    echo -e '  \033[01;33m'\"${HOME}/.bash/inputrc.orig\"'\033[00m'
+  else
+    ln -s ${HOME}/.bash/inputrc ${HOME}/.inputrc
+  fi
 else
-  git clone https://github.com/TNaky/bashrc.git ${HOME}/.bash
-fi
-
-if [ -f "${HOME}/.bashrc" ]; then
-  mv ${HOME}/.bashrc ${HOME}/.bash/bashrc.orig
-  ln -s ${HOME}/.bash/bashrc ${HOME}/.bashrc
-  echo "Original .bashrc file has been moved to"
-  echo -e '  \033[01;33m'\"${HOME}/.bash/bashrc.orig\"'\033[00m'
-else
-  ln -s ${HOME}/.bash/bashrc ${HOME}/.bashrc
-fi
-
-if [ -f "${HOME}/.bash_profile" ]; then
-  mv ${HOME}/.bash_profile ${HOME}/.bash/bash_profile.orig
-  ln -s ${HOME}/.bash/bash_profile ${HOME}/.bash_profile
-  echo "Original .bash_profile file has been moved to"
-  echo -e '  \033[01;33m'\"${HOME}/.bash/bash_profile.orig\"'\033[00m'
-else
-  ln -s ${HOME}/.bash/bash_profile ${HOME}/.bash_profile
-fi
-
-if [ -f "${HOME}/.inputrc " ]; then
-  mv ${HOME}/.inputrc ${HOME}/.bash/inputrc.orig
-  ln -s ${HOME}/.bash/inputrc ${HOME}/.inputrc
-  echo "Original .inputrc file has been moved to"
-  echo -e '  \033[01;33m'\"${HOME}/.bash/inputrc.orig\"'\033[00m'
-else
-  ln -s ${HOME}/.bash/inputrc ${HOME}/.inputrc
+  echo "Please git install"
+  echo -e '  \033[00;33m'brew install git'\033[00m'
+  echo -e '  \033[00;33m'sudo aptitude install git'\033[00m'
 fi
